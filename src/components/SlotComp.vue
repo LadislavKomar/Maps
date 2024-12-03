e8cef7794837b2efa5704483d7508a0fb245a0f5
LadislavKomar <komar.ladislav@gmail.com>
2024-12-03 09:43:31 +0000

    Initial commit

---
+++ src/components/SlotComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div> <!-- This div makes the card-like appearance -->
+    <slot></slot>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+  div {
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    border-radius: 10px;
+    margin: 10px;
+  }
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,11 @@
+<template>
+  <h3>Slots in Vue</h3>  
+  <p>We create card-like div boxes from the foods array.</p>
+  <div id="wrapper">
+    <slot-comp v-for="x in foods">
+      <img v-bind:src="x.url">
+      <h4>{{x.name}}</h4>
+      <p>{{x.desc}}</p>
+    </slot-comp>
+  </div>
+</template>
